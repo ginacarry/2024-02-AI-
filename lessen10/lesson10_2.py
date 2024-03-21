ad9f59c50fbeed7d31e82b475eb4047c0172b7d3
石婕妤 <160602734+ginacarry@users.noreply.github.com>
2024-03-21 13:07:40 +0000

    007

--- lessen10/lesson10_2.py
+++ lessen10/lesson10_2.py
@@ -1,8 +1,10 @@
 from tools_2 import get_names
+import pyinputplus as pyip
 
 def main():
-    names:list[str] = get_names()
+    nums:int = pyip.inputInt("請輸入學生數(1~30):",min=1,max=30)
+    names:list[str] = get_names(nums=nums)
     print(names)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
